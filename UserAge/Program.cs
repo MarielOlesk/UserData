51e86e1a66a18831f3b3f93eaba47d8ce6692889
Indrek Täht <indrek.taht@gmail.com>
2022-02-04 10:28:57 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis
+            //"oled sündinud aastal {yearOfBirth};
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userage = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - userage; //aastaks 2021 pantud, sest hetkel alles aasta algus
+
+            Console.WriteLine("Oled sündinud aastal " + yearOfBirth + ".");
+
+        }
+    }
+}
---
+++ UserData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolist;
+            //toint32 konventeerib numbriks ehk arvuks, et ta mahuks int muutuja sisse
+
+            Console.WriteLine("Palun sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //string interpolation - võimaldab kirjutada täislauset ja sisestada sinna muutujat otseselt, ilma et sisestada + kasutada
+
+            Console.WriteLine($"Oled {userAge} aastat vana");
+
+           //NB: Console.WriteLine($"Oled userAge aastat vana") kuvab mulle täpselt sellise teksti:"oled userAge aastat vana"
+           // See ei ole õige, korrektse jaoks on vaja kirjutada loogilised sulud {userAge} ümber
+           // võib kasutada mõlemat varianti + või $ viisi, oleneb eelistusest. 
+
+
+
+
+        }
+    }
+}
